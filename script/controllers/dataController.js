8f7dd0a55bff4fa2874106870ed831d5417d5604
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-11-06 13:06:44 +0800

    改掉BUG

--- script/controllers/dataController.js
+++ script/controllers/dataController.js
@@ -13,26 +13,9 @@ const getDataReport = async (req, res, next) => {
         const queryOptions = generateQueryOptions(params);
 
         // 執行查詢，從子訂單中統計銷售數量和銷售額
-        const reportData = await SubOrder.findAll({
-            ...queryOptions,
-            include: [
-                {
-                    model: MenuItem,
-                    attributes: ['MenuItemName'],
-                    through: { model: SubOrderItem, attributes: [] } // 僅關聯，不再獲取額外數據
-                }
-            ],
-            attributes: [
-                // 分隔改成周月
+        const reportData = await SubOrder.findAll({});
 
-                [fn('SUM', col('SubOrderItems.quantity')), 'salesQuantity'], // 計算銷售數量
-                [fn('SUM', fn('MULTIPLY', col('SubOrderItems.quantity'), col('SubOrderItems.price'))), 'sales'] // 計算總銷售額
-            ],
-            group: queryOptions.group, // 使用生成的主要分組鍵
-            order: [['timeUnit', 'ASC']]
-        });
-
-        res.status(200).json({ message: '報告生成成功', data: reportData });
+        res.status(200).json(reportData);
     } catch (error) {
         next(error);
     }
